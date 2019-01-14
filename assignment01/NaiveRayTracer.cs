959a1675b0ffa97ba2e3b05b901e885f2a4245cb
Wei Jiang <weijiang@Weis-MacBook-Pro.local>
2019-01-14 00:54:32 -0800

    minor

--- assignment01/NaiveRayTracer.cs
+++ assignment01/NaiveRayTracer.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 namespace Assignment01
 {
     public class NaiveRayTracer : MonoBehaviour
@@ -66,127 +67,6 @@ namespace Assignment01
                 throw new NotImplementedException();
             renderedResult.Apply();
         }
-
-        // private Texture2D GenCheckboard(int width, int height)
-        // {
-        //     /*
-        //     generate a checkboard pattern on a texture
-        //     and return the texture.
-
-        //     int width - width of the returned texture
-        //     int height - height of the return texture
-        //     return:
-        //         Texture2D - Texture2D object which contains the rendered result
-        //     */
-        //     Texture2D checkboardResult = new Texture2D(width, height);
-        //     #region Generate a black and white checker pattern
-        //     // in frame coordinates
-        //     // x axis grow from left to right
-        //     // y axis grow from lower to upper
-        //     for (int y = 0; y < canvasHeight; ++y)
-        //     {
-        //         for (int x = 0; x < canvasWidth; ++x)
-        //         {
-        //             int blockSize = 100;
-        //             int xblock = x / blockSize;
-        //             int yblock = y / blockSize;
-        //             checkboardResult.SetPixel(x, y,
-        //                 (xblock + yblock) % 2 == 0 ? new Color(255, 0, 0) : Color.white);
-        //         }
-        //     }
-        //     checkboardResult.Apply();
-        //     #endregion
-
-        //     return checkboardResult;
-        // }
-
-        // private Texture2D GenSphere(int width, int height)
-        // {
-        //     /*
-        //     implement ray-sphere intersection and render a sphere with ambient, diffuse and specular lighting.
-        //     you can define the sphere center and radius in this function.
-
-        //     int width - width of the returned texture
-        //     int height - height of the return texture
-        //     return:
-        //         Texture2D - Texture2D object which contains the rendered result
-        //     */
-        //     throw new NotImplementedException();
-        // }
-
-        // private Texture2D GenBarycentricVis(int width, int height)
-        // {
-        //     /*
-        //     implement ray-triangle intersection and 
-        //     visualize the barycentric coordinate on each of the triangles of a cube, 
-        //     with Red, Green and Blue for each coordinate.
-        //     you can define your cube vertices and indices in this function.
-
-        //     int width - width of the returned texture
-        //     int height - height of the return texture
-        //     return:
-        //         Texture2D - Texture2D object which contains the rendered result
-        //     */
-        //     throw new NotImplementedException();
-        // }
-
-        // private Texture2D GenUVMapping(int width, int height)
-        // {
-        //     /*
-        //     implement UV mapping with the calculated barycentric coordinate in the previous step, 
-        //     and visualize a texture image on each face of the cube.
-        //     (choose any texture you like)
-        //     we have declared textureOnCube as a public variable,
-        //     you can attach texture to it from Unity.
-        //     you can define your cube vertices and indices in this function.
-
-        //     int width - width of the returned texture
-        //     int height - height of the return texture
-        //     return:
-        //         Texture2D - Texture2D object which contains the rendered result
-        //     */
-        //     throw new NotImplementedException();
-        // }
-
-        // private bool IntersectSphere(Vector3 origin,
-        //                                 Vector3 direction,
-        //                                 Vector3 sphereCenter,
-        //                                 float sphereRadius,
-        //                                 out float t,
-        //                                 out Vector3 intersectNormal)
-        // {
-        //     /*
-        //     Vector3 origin - origin point of the ray
-        //     Vector3 direction - the direction of the ray
-        //     Vector3 sphereCenter - center of target sphere
-        //     float sphereRadius - radius of target sphere
-        //     out float t - distance the ray travelled to hit a point
-        //     out Vector3 intersectNormal - normal of the hit point
-        //     return:
-        //         bool - indicating hit or not
-        //     */
-        //     throw new NotImplementedException();
-        // }
-
-        // private bool IntersectTriangle(Vector3 origin,
-        //                                 Vector3 direction,
-        //                                 Vector3 vA,
-        //                                 Vector3 vB,
-        //                                 Vector3 vC,
-        //                                 out float t,
-        //                                 out Vector3 barycentricCoordinate)
-        // {
-        //     /*
-        //     Vector3 origin - origin point of the ray
-        //     Vector3 direction - the direction of the ray
-        //     vA, vB, vC - 3 vertices of the target triangle
-        //     out float t - distance the ray travelled to hit a point
-        //     out Vector3 barycentricCoordinate - you should know what this is
-        //     return:
-        //         bool - indicating hit or not
-        //     */
-        //     throw new NotImplementedException();
-        // }
 
         // Update is called once per frame
         void Update()
---
+++ assignment01/readme.md
@@ -0,0 +1,9 @@
+NaiveRayTracer.cs is script attached to the camera object.
+
+CheckboardGenerator.cs is the template script that generate a checkboard pattern.
+
+SphereGenerator.cs is the skeleton code for rendering a sphere using ray tracing.
+
+CubeGenerator.cs is the skeleton code for rendering a cude with barycentric coordinates visualization and texture mapping.
+
+nyancat.jpeg is sample texture we provided.
