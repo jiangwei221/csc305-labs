968907652df6b055c2873827b3789cacb14b8352
Wei Jiang <weijiang@w87-161-160.wireless.uvic.ca>
2019-03-05 12:59:02 -0800

    assignment02-help lab

---
+++ assignment02_help/ParabolaSurface.cs
@@ -0,0 +1,95 @@
+﻿/*
+UVic CSC 305, 2019 Spring
+
+Helping lab for assignment02
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParabolaSurface : MonoBehaviour {
+
+    public Material simpleMaterial;
+
+	// Use this for initialization
+	void Start () {
+        MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+        //renderer.material = simpleMaterial;
+        GenerateParabolaSurface();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GenerateParabolaSurface()
+    {
+        Mesh mesh = gameObject.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        int[] indices = mesh.triangles;
+        Vector2[] uvs = mesh.uv;
+
+        mesh.Clear();
+
+        //subdivision = how many squares per row/col
+        int subdivision = 100;
+        int stride = subdivision + 1;
+        int num_vert = stride * stride;
+        int num_tri = subdivision * subdivision * 2;
+
+        indices = new int[num_tri * 3];
+        int index_ptr = 0;
+        for (int i = 0; i < subdivision; i++)
+        {
+            for (int j = 0; j < subdivision; j++)
+            {
+                int quad_corner = i * stride + j;
+                indices[index_ptr] = quad_corner;
+                indices[index_ptr+1] = quad_corner+stride;
+                indices[index_ptr+2] = quad_corner+stride+1;
+                indices[index_ptr+3] = quad_corner;
+                indices[index_ptr+4] = quad_corner+stride+1;
+                indices[index_ptr+5] = quad_corner+1;
+                index_ptr += 6;
+            }
+        }
+
+        Debug.Assert(index_ptr == indices.Length);
+
+        const float xz_start = -5;
+        const float xz_end = 5;
+        float step = (xz_end - xz_start) / (float)(subdivision);
+        vertices = new Vector3[num_vert];
+        uvs = new Vector2[num_vert];
+
+        for (int i = 0; i < stride; i++)
+        {
+            for (int j = 0; j < stride; j++)
+            {
+                // notice the bahavior here
+                bool show_backface = false;
+                float cur_x;
+                float cur_z;
+                //i don't know how this happened(showing back faces)
+                if (show_backface)
+                {
+                    cur_x = xz_start + i * step;
+                    cur_z = xz_start + j * step;
+                }
+                else
+                {
+                    cur_x = xz_start + j * step;
+                    cur_z = xz_start + i * step;
+                }
+                float cur_y = (-(cur_x * cur_x + cur_z * cur_z) / (float)10.0)+5;
+                vertices[i * stride + j] = new Vector3(cur_x, cur_y, cur_z);
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.triangles = indices;
+        mesh.RecalculateNormals();
+    }
+}
---
+++ assignment02_help/simpleShader.shader
@@ -0,0 +1,73 @@
+﻿//UVic CSC 305, 2019 Spring
+//
+//Helping lab for assignment02
+
+Shader "Unlit/simpleMaterial"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+	}
+
+		SubShader
+		{
+			Tags { "RenderType" = "Opaque"
+			"LightMode" = "ForwardBase" }
+			LOD 100
+
+			Pass
+			{
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+
+				#include "UnityCG.cginc"
+				#include "Lighting.cginc"
+
+				struct appdata
+				{
+					float4 vertex : POSITION;
+					float2 uv : TEXCOORD0;
+					float3 normal : NORMAL;
+
+				};
+
+				struct v2f
+				{
+					float2 uv : TEXCOORD0;
+					float height : TEXCOORD1;
+					float3 normal : TEXCOORD2;
+					float4 worldpos : TEXCOORD3;
+					float4 vertex : SV_POSITION;
+
+				};
+
+				sampler2D _MainTex;
+				float4 _MainTex_ST;
+
+				v2f vert(appdata v)
+				{
+					v2f o;
+					o.vertex = UnityObjectToClipPos(v.vertex);
+					o.height = v.vertex.y;
+					o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+					o.normal = UnityObjectToWorldNormal(v.normal);
+					o.worldpos = mul(unity_ObjectToWorld, v.vertex);
+					return o;
+				}
+
+				float4 frag(v2f i) : SV_Target
+				{
+
+					float4 fragment_color = float4(0,0,0,0);
+					if (i.height < 1) fragment_color = float4(1,0,0,0);
+					else if (i.height < 2) fragment_color = float4(0,1,0,0);
+					else if (i.height < 3) fragment_color = tex2D(_MainTex, i.uv);//float4(0,0,1,0);
+					else fragment_color = float4(1,1,0,0);
+
+					return fragment_color;
+				}
+				ENDCG
+			}
+		}
+}
