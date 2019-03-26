2ec1b00abd3053d0a69ae88f6c61d241fc8b5604
Wei Jiang <weijiang@w87-161-197.wireless.uvic.ca>
2019-03-26 13:40:12 -0700

    assignment03 help

---
+++ assignment03_help/ratating_cubs/CircleMovement.cs
@@ -0,0 +1,23 @@
+﻿/*
+UVic CSC 305, 2019 Spring
+
+Helping lab for assignment03
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMovement : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(0.0f, 5.0f, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+    }
+}
